5c93dc242e5d73982edde652b7938f93db3934c2
Adejayan Ebunoluwa <98878080+Tsegalion@users.noreply.github.com>
2023-10-16 15:17:03 +0100

    Update Load to db code.py

--- Load to db code.py
+++ Load to db code.py
@@ -1,19 +1,9 @@
+# Note this is continuation from the transform code so variable or tables names are the same****
 # import libraries
-import pandas as pd
 from sqlalchemy import create_engine
 
 # Connect to postgres DB
 engine = create_engine("postgresql+psycopg2://Tsega:*******@localhost:5432/DataProjects")
-
-
-# Read each CSV file into a pandas DataFrame
-icos = pd.read_csv(r"C:\Users\Tsega\ICO Project\data\icos(Fact Table).csv")
-category = pd.read_csv(r"C:\Users\Tsega\ICO Project\data\category(DimT).csv")
-platform = pd.read_csv(r"C:\Users\Tsega\ICO Project\data\platform(DimT).csv")
-project = pd.read_csv(r"C:\Users\Tsega\ICO Project\data\project(DimT).csv")
-token_role = pd.read_csv(r"C:\Users\Tsega\ICO Project\data\token_role(DimT).csv")
-date = pd.read_csv(r"C:\Users\Tsega\ICO Project\data\date(DimT).csv")
-
 
 # Load each DataFrame into the PostgreSQL database
 date.to_sql('date', engine, if_exists='append', index=False)
